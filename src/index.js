52f02b9a57aa33a30b6a4b9f806d6705a49c7de6
Ahmed <ahmed@fostr.io>
2022-01-24 16:56:02 +0000

    initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const favouriteTeam = {
+    team: "Liverpool",
+    formation: "4-3-3",
+    manager: "Klopp"
+}
+
+const favouriteTeamInfo = {
+    ...favouriteTeam,
+    formation: "5-3-2",
+    stadium: "Anfield"
+}
+
+
+console.log(favouriteTeam)
+console.log(favouriteTeamInfo)
